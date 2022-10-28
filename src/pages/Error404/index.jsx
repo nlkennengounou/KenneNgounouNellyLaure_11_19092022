35b9bc14ca50f4a7bfdab5d6473a7ab027188877
nlkennengounou <84591282+nlkennengounou@users.noreply.github.com>
2022-10-28 03:19:32 +0200

    Etape6 -page d’erreur

--- src/pages/Error404/index.jsx
+++ src/pages/Error404/index.jsx
@@ -1,9 +1,21 @@
+import { Link } from "react-router-dom";
+
 function Error404() {
-    return (
-        <div>
-            Error 404
+  return (
+   
+    <div className="error">
+        <div className="error__description">
+            <p className="error__code">404</p>
+            <p className="error__message">
+            Oups! La page que vous demandez n'existe pas.
+            </p>
         </div>
-    )
+        <Link to="/" className="error__link">
+            Retourner sur la page d'accueil
+        </Link>
+    </div>
+
+  );
 }
 
 export default Error404;
